31c569a7f68cca1f8cc4b75537cf508857f939fd
Felipe Brandão de Figueiredo <128430670+FelipeBrandaoFigueiredo@users.noreply.github.com>
2024-03-19 21:20:15 -0300

    Add files via upload

---
+++ Desafio_1/Desafio_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        int INDICE, SOMA, K;
+
+        INDICE = 13;
+
+        K = 0;
+
+        // Chamando método para fazer o calculo da soma.
+        SOMA = CalcularSoma(INDICE, K);
+
+        Console.WriteLine($"{INDICE} É o valor do indice.\n");
+
+        Console.WriteLine($"O valor da variável SOMA é: {SOMA}");
+
+
+    }
+
+    /*
+     * MÉTODO CALCULO SOMA
+     */
+    static int CalcularSoma (int indice, int k)
+    {
+        int soma = 0;
+
+        while (k < indice)
+        {
+            k++;
+            soma += k;
+        }
+        return soma;
+    }
+
+
+}
